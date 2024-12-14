11871af4ff485a4a8363146cd6d2f13ba8939e0f
Akhilesh <akhileshraj91@gmail.com>
2024-12-14 17:18:40 +0530

    changes

--- simulation_vm/simulation/remote_io/modbus/run_all.sh
+++ simulation_vm/simulation/remote_io/modbus/run_all.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
 sim_path=/home/vagrant/GRFICSv2/simulation_vm/simulation/remote_io/modbus
+<<<<<<< HEAD
+=======
+sudo pkill simulation
+./../../simulation &
+>>>>>>> 464fd55 (recent updates)
 sudo pkill python3
 sudo python3 $sim_path/feed1.py &
 sudo python3 $sim_path/feed2.py &
 sudo python3 $sim_path/purge.py &
 sudo python3 $sim_path/product.py &
 sudo python3 $sim_path/tank.py &
+<<<<<<< HEAD
 sudo python3 $sim_path/analyzer.py &
+=======
+sudo python3 $sim_path/analyzer.py &
+>>>>>>> 464fd55 (recent updates)
